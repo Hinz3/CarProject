8e711a5eecb4a5bc142cff3b41419bbf62c5c3c5
Jacob Hinze <kontakt@jacobhinze.dk>
2019-12-03 19:24:13 +0100

    Added cors to header

--- Backend/index.js
+++ Backend/index.js
@@ -66,6 +66,17 @@ client.on('connect', function() { // When connected
     });
 });
 
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+});
+
 app.use('/api', routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
